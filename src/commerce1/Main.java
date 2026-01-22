c9eec6a540e378d72dbabb93c0fdc0e792ab951d
eunjiom <norgg021@gmail.com>
2026-01-22 19:33:08 +0900

    Step3: 카테고리 안에 상품 담고 리스트로 묶고 넘기기

--- src/commerce1/Main.java
+++ src/commerce1/Main.java
@@ -10,42 +10,48 @@ public class Main {
         // 리스트 선언, product 추가
         List<Product> products = new ArrayList<>();
 
-        // 제품 세팅
-        products.add(new Product(
+        // 카테고리 생성
+        Category electronics = new Category("전자제품");
+        Category cloth = new Category("의류");
+        Category food = new Category("식품");
+
+        // 카테고리에 상품 담기
+        electronics.getProducts().add(new Product(
                 "Galaxy S25",
                 1200000,
                 "최신 안드로이드 스마트폰",
                 10
         ));
 
-        products.add(new Product(
+        electronics.getProducts().add(new Product(
                 "iPhone 16",
                 1350000,
                 "Apple의 최신 스마트폰",
                 5
         ));
 
-        products.add(new Product(
+        electronics.getProducts().add(new Product(
                 "MacBook Pro",
                 2400000,
                 "M3 칩셋이 탑재된 노트북",
                 7
         ));
 
-        products.add(new Product(
+        electronics.getProducts().add(new Product(
                 "AirPods Pro",
                 350000,
                 "노이즈 캔슬링 무선 이어폰",
                 13
         ));
 
-        // 카테고리 생성
-        Category electronics = new Category("전자제품");
-        Category cloth = new Category("의류");
-        Category food = new Category("식품");
+        // 카테고리 리스트로 묶기
+        List<Category> categories = new ArrayList<>();
+        categories.add(electronics);
+        categories.add(cloth);
+        categories.add(food);
 
-        // start() 함수
-        CommerceSystem system = new CommerceSystem(products);
+        // start() 함수 / 커머스 시스템으로 넘기기
+        CommerceSystem system = new CommerceSystem(categories);
         system.start();
 
     }

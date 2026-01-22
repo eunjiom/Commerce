c1405caff879b563f73ce2626911c7e136283407
eunjiom <norgg021@gmail.com>
2026-01-22 15:09:07 +0900

    Step1: 제품 클래스 생성, 메인 클래스에 제품 추가

---
+++ src/Commerce1/Main.java
@@ -0,0 +1,42 @@
+package Commerce1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static void main() {
+
+        // 리스트 선언, product 추가
+        List<commerce1.Product> products = new ArrayList<>();
+
+        // 제품 세팅
+        products.add(new commerce1.Product(
+                "Galaxy S25",
+                1200000,
+                "최신 안드로이드 스마트폰",
+                10
+        ));
+
+        products.add(new commerce1.Product(
+                "iPhone 16",
+                1350000,
+                "Apple의 최신 스마트폰",
+                5
+        ));
+
+        products.add(new commerce1.Product(
+                "MacBook Pro",
+                2400000,
+                "M3 칩셋이 탑재된 노트북",
+                7
+        ));
+
+        products.add(new commerce1.Product(
+                "AirPods Pro",
+                350000,
+                "노이즈 캔슬링 무선 이어폰",
+                13
+        ));
+
+    }
+}
---
+++ src/Commerce1/Product.java
@@ -0,0 +1,35 @@
+package Commerce1;
+
+public class Product {
+
+    // 상품명, 가격, 설명, 재고수량 필드 만들기
+    private String name;
+    private int price;
+    private String description;
+    private int stock;
+
+    // 생성자 만들기
+    public Product(String name, int price, String description, int stock){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.stock = stock;
+    }
+
+    // getter로 정보 출력
+    public String getName(){
+        return name;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getStock(){
+        return stock;
+    }
+}
